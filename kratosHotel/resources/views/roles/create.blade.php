16f6d3e86d4228fb0ce95752e01fe8b615321936
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-11-14 22:08:43 -0500

    kratos1.0

---
+++ kratosHotel/resources/views/roles/create.blade.php
@@ -0,0 +1,24 @@
+@extends ('layout.app')
+
+    @section('content')
+    <form class="container-fluid col-8" method="POST" >
+        @csrf
+    <div class="form-group">
+        <label for="exampleInputEmail1">Id_ocupacion</label>
+        <input name="id_ocupacion" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa el id">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Descripcion</label>
+        <input name="descripcion" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa la descripcion">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Funciones</label>
+        <input name="funciones" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa las funciones">
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    @endsection
